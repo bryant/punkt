5ec2b87f77cd4be6fe5c2f79cc1674bf282e348a
bryant <bryant@defrag.in>
2014-09-22 14:18:39 -0400

    find_breaks

--- NLP/Hunkt.hs
+++ NLP/Hunkt.hs
@@ -202,33 +202,22 @@ classify_by_type tok@(Token {entity=(Word w)})
     | otherwise = return tok
 classify_by_type tok = return tok
 
-classify_by_next :: (Token, Token) -> Punkt Token
-classify_by_next (this, Token _ _ (Word next) _ _)
+classify_by_next :: Token -> Token -> Punkt Token
+classify_by_next this (Token _ _ (Word next) _ _)
     | entity this == Ellipsis || abbrev this = do
         ortho_says <- decide_ortho next
         prob_says <- prob_starter next
         return $ case ortho_says of
             Nothing -> this { sentend = prob_says >= 30 }
             Just bool -> this { sentend = bool }
-classify_by_next (this, _) = return this
+classify_by_next this _ = return this
 
-coeur :: Text -> [Token]
-coeur corpus = Reader.runReader c punkt
+find_breaks :: Text -> [Int]
+find_breaks corpus = Reader.runReader find_breaks punkt
     where
     toks = to_tokens corpus
     punkt = build_punkt_data toks
-    c = mapM classify_by_type toks >>= mapM classify_by_next . to_pairs
-    to_pairs xs = zip xs $ drop 1 xs
-
-{-
---split_sentences :: Text -> Text
-split_sentences corpus =
-    let
-    stats = build_punkt_data corpus
-    toks =
-        map (refine
-        map (classify_by_type (type_count stats) (total_toks stats)) $
-        to_tokens corpus
-    in
-    toks
--}
+    find_breaks = do
+        abbrd <- mapM classify_by_type toks
+        final <- Reader.zipWithM classify_by_next abbrd (drop 1 abbrd)
+        return $ map (\t -> offset t + toklen t) (filter sentend final)
